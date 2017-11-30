e7a7171f8d955c956de466c16dae1b597a668a4b
ziad <ziad.jb@gmail.com>
2017-11-30 10:05:47 +0100

    maj ecommerce

--- ecommerce/panier.php
+++ ecommerce/panier.php
@@ -1,4 +1,8 @@
 <?php
 require_once( 'includes/init.inc.php' );
 
+// creer un table HTML representant le panier
+// avec suppression d'un produit (bouton X sur la ligne du produit)
+// avec suppression de l'ensemble du panier (bouton vider le panier)
+
 dd($_SESSION);
