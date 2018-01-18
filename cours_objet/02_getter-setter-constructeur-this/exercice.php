c4b3ee1396d37672654b3d2d6d90c93f4a88eb6e
ziad <ziad.jb@gmail.com>
2018-01-18 12:28:47 +0100

    correction

--- cours_objet/02_getter-setter-constructeur-this/exercice.php
+++ cours_objet/02_getter-setter-constructeur-this/exercice.php
@@ -1,38 +1,40 @@
 <?php
 //--------------------------------------------
+
 /************************
-UML:
------------------
-|    Vehicule	|
------------------
-|	$litres		  |
------------------
-|setLitres()    |
-|getLitres()	  |
------------------
-
------------------
-|    Pompe   	|
------------------
-|	$stock		  |
------------------
-|setStock()     |
-|getStock() 	  |
-|donnerEssence()|
------------------
-
-1. Création d'un véhicule 1
-2. Attribuer un nombre de litres d'essence au vehicule 1 : 5
-3. Afficher le nombre de litres d'essence du vehicule 1
-4. Création d'une pompe
-5. Attribuer un stock d'essence à la pompe : 800
-6. Afficher le stock d'essence de la pompe
-7. la pompe donne de l'essence en faisant le plein (50L) à la voiture1
-8. Afficher le nombre de litres d'essence pour la voiture1 après ravitaillement
-9. Afficher le stock d'essence restant pour la pompe
-10. Faire en sorte que le véhicule ne puisse pas contenir plus de 50L d'essence (limite reservoir).
+ * UML:
+ * -----------------
+ * |    Vehicule  |
+ * -----------------
+ * |  $litres      |
+ * -----------------
+ * |setLitres()    |
+ * |getLitres()    |
+ * -----------------
+ *
+ * -----------------
+ * |    Pompe    |
+ * -----------------
+ * |  $stock      |
+ * -----------------
+ * |setStock()     |
+ * |getStock()    |
+ * |donnerEssence()|
+ * -----------------
+ *
+ * 1. Création d'un véhicule 1
+ * 2. Attribuer un nombre de litres d'essence au vehicule 1 : 5
+ * 3. Afficher le nombre de litres d'essence du vehicule 1
+ * 4. Création d'une pompe
+ * 5. Attribuer un stock d'essence à la pompe : 800
+ * 6. Afficher le stock d'essence de la pompe
+ * 7. la pompe donne de l'essence en faisant le plein (50L) à la voiture1
+ * 8. Afficher le nombre de litres d'essence pour la voiture1 après ravitaillement
+ * 9. Afficher le stock d'essence restant pour la pompe
+ * 10. Faire en sorte que le véhicule ne puisse pas contenir plus de 50L d'essence (limite reservoir).
  ***********************/
-class Vehicule {
+class Vehicule
+{
 	protected $litres;
 
 	/**
@@ -42,7 +44,7 @@ class Vehicule {
 	 */
 	public function setLitres(int $litres): void
 	{
-		if(is_numeric($litres) && $litres <= 50) {
+		if (is_numeric($litres) && $litres <= 50) {
 			$this->litres = $litres;
 		} else {
 			echo "Le véhicule doit posséder maximum 50 litres et le type doit être un chiffre<hr>";
@@ -62,8 +64,8 @@ class Vehicule {
 	}
 }
 
-
-class Pompe {
+class Pompe
+{
 	protected $stock;
 
 	/**
@@ -74,7 +76,7 @@ class Pompe {
 	// en PHP 7 nous avons la possibilité de typer nos valeurs de retour. Dans ma méthode, ici, j'indique que je ne retourne rien (pas de réponse de la méthode), donc on indique "void".
 	public function setStock(int $stock): void
 	{
-		if(is_numeric($stock)) {
+		if (is_numeric($stock)) {
 			$this->stock = $stock;
 		} else {
 			echo "le stock doit être numérique";
@@ -100,13 +102,13 @@ class Pompe {
 	 */
 	public function donnerEssence(Vehicule $vehicule): int
 	{
-		$dansVoiture = $vehicule->getLitres(); // je recupere le nombre de litres dispo dans la voiture
-		$stockPompe = $this->getStock(); // je prend le stock actuel de la pompe
+		$dansVoiture    = $vehicule->getLitres(); // je recupere le nombre de litres dispo dans la voiture
+		$stockPompe     = $this->getStock(); // je prend le stock actuel de la pompe
 		$essenceADonner = 50 - $dansVoiture; // donne 45
 
 		$plein = $essenceADonner + $dansVoiture;
 		// je verifie si $plein ne depasse pas 50L avant de faire les affectations (question 8)
-		if($plein <= 50) {
+		if ($plein <= 50) {
 			$this->setStock($stockPompe - $essenceADonner); // 800 - 45 = 755
 			$vehicule->setLitres($plein); // 45 + 5 = 50
 		}
@@ -124,7 +126,6 @@ $voiture->setLitres(5);
 
 //3
 echo $voiture->getLitres() . ' L d\'essence pour le véhicule 1<hr>';
-
 
 //4
 $pompe = new Pompe();
@@ -144,7 +145,6 @@ echo "Après ravitaillement : " . $voiture->getLitres() . "L<hr>";
 
 //9
 echo "Stock restant pour la pompe : " . $pompe->getStock() . "L<hr>";
-
 
 //---------------
 $voiture2 = new Vehicule();
